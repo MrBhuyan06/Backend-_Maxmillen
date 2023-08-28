f82c100ef388eb93c9c45b84548a6c649c3c48f1
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-08-28 23:32:31 +0530

    Adde the test successfully

--- Assigment/Assigment_1/My code/app.js
+++ Assigment/Assigment_1/My code/app.js
@@ -1,12 +1,17 @@
 //create a server
 const { log } = require("console");
 const app = require("http");
+const fs = require("fs");
 const { text } = require("stream/consumers");
+const { Stream } = require("stream");
 
 const server = app.createServer((req, res) => {
   //create the server
   //   res.write("<h1>Hello</h1>");
   const url = req.url;
+  //   console.log(url);
+  const method = req.method;
+  //   console.log(method);
   //   console.log(url);
   if (url === "/") {
     res.write("<html>");
@@ -14,7 +19,7 @@ const server = app.createServer((req, res) => {
     res.write("</head>");
 
     res.write(
-      '<boby><form method="post" action="/create_user"><input type="text"/> <button type="submit"> send</button> </form></boby>'
+      '<boby><form method="POST" action="/create_user"><input type="text"/name="name"> <button type="submit"> send</button> </form></boby>'
     );
 
     res.write("</html>");
@@ -29,8 +34,23 @@ const server = app.createServer((req, res) => {
 
     res.write("</html>");
     return res.end();
+  } else if (url === "/create_user" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      let name = parseBody.split("=")[1];
+      console.log(name);
+
+      fs.writeFile("name.txt", name, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
-  else if()
 });
 const port = 3000;
 server.listen(port, (req, res) => {
